75c34c501211b1605c19c363cbf0bdce6bcb9417
alfreeemr <143364380+alfreeemr@users.noreply.github.com>
2023-08-28 03:10:45 +0200

    Delete FrediCard directory

--- FrediCard/index/jugar.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("jugar-btn").addEventListener("click", function () {
-  // Redireccionar a la página de selección de jugadores
-  window.location.href = "../seleccion_jugadores/index.html";
-});
